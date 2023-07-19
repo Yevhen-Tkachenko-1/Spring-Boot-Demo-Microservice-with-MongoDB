7860e1dc40a286d49cd3e1c3f8abb36c67099c6c
Yevhen_Tkachenko <Yevhen_Tkachenko@epam.com>
2023-07-19 11:09:00 +0300

    initialize Spring project

---
+++ src/main/java/com/yevhent/microservicedemo/SpringBootMicroservice.java
@@ -0,0 +1,13 @@
+package com.yevhent.microservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservice {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservice.class, args);
+	}
+
+}
---
+++ src/test/java/com/yevhent/microservicedemo/SpringBootMicroserviceTests.java
@@ -0,0 +1,13 @@
+package com.yevhent.microservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
